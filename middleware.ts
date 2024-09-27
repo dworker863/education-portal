f8c566b778adfab7286f03998318d997b3fa8c5d
dworker <dworker863@gmail.comm>
2024-09-27 15:07:42 +0500

    Added middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,39 @@
 import NextAuth from 'next-auth';
 import authConfig from './auth.config';
+import {
+  apiAuthPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+} from './app/libs/routes';
 
-export const { auth: middleware } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const pathname = req.nextUrl.pathname;
+  console.log('ROUTE: ', pathname);
+
+  const isApiAuthRoute = pathname.startsWith(apiAuthPrefix);
+  const isAuthRoute = authRoutes.includes(pathname);
+  const isPublicRoute = publicRoutes.includes(pathname);
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, req.nextUrl));
+    }
+  } else if (!isPublicRoute && !isApiAuthRoute && !isLoggedIn) {
+    console.log(pathname);
+
+    return Response.redirect(new URL('/signin', req.nextUrl));
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};

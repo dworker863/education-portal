fd6fd876f2e9c6d214ca5a3aa76ae03fcc993e52
dworker <dworker863@gmail.com>
2024-10-21 21:25:31 +0500

    Added ExerciseForm

--- app/components/ExerciseForm.tsx
+++ app/components/ExerciseForm.tsx
@@ -1,17 +1,150 @@
-import React from 'react';
+'use client';
+
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { addExerciseSchema } from '../libs/validation';
 import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import ErrorMessage from './ErrorMessage';
 
 const ExerciseForm = () => {
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
   const form = useForm<z.infer<typeof addExerciseSchema>>({
     resolver: zodResolver(addExerciseSchema),
-    // defaultValues: {
-    //   name: '',
-    // },
+    defaultValues: {
+      name: '',
+      task: '',
+      code: '',
+      test: '',
+      solution: '',
+      requiredRank: '',
+      prizePoints: 0,
+    },
   });
-  return <Form></Form>;
+
+  const onSubmit = (values: z.infer<typeof addExerciseSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="name" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="task"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Task</FormLabel>
+              <FormControl>
+                <Input placeholder="task" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="code"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Code</FormLabel>
+              <FormControl>
+                <Input placeholder="code" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="test"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Test</FormLabel>
+              <FormControl>
+                <Input placeholder="test" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="solution"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Solution</FormLabel>
+              <FormControl>
+                <Input placeholder="solution" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="requiredRank"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Required Rank</FormLabel>
+              <FormControl>
+                <Input placeholder="requiredRank" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="prizePoints"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Prize Points</FormLabel>
+              <FormControl>
+                <Input placeholder="prizePoints" {...field} />
+              </FormControl>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {error && <ErrorMessage message={error} />}
+        {success && <ErrorMessage message={success} />}
+        <Button type="submit">Add Exercise</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default ExerciseForm;
--- app/course/[name]/[lesson]/page.tsx
+++ app/course/[name]/[lesson]/page.tsx
@@ -1,9 +1,11 @@
 import Editor from '@/app/components/Editor';
+import ExerciseForm from '@/app/components/ExerciseForm';
 
 export default function Lesson({ params }: { params: { lesson: string } }) {
   return (
     <>
       <h1>Lesson: {params.lesson}</h1>
+      <ExerciseForm />
       <div className="flex w-full px-10">
         <div className="w-2/4 text-center">Content</div>
         <div className="w-2/4 text-center">

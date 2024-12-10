ef5c72e3595e40ed6a9ebc8716319b60aecd4599
dworker <dworker863@gmail.com>
2024-12-10 23:51:37 +0500

    Added styles to error and success message components

--- app/components/error-message.tsx
+++ app/components/error-message.tsx
@@ -1,11 +1,17 @@
-import React, { FC } from 'react';
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
+import { FC } from 'react';
 
 type TErrorMessageProps = {
   message: string;
 };
 
 const ErrorMessage: FC<TErrorMessageProps> = ({ message }) => {
-  return <p className="text-red-600">{message}</p>;
+  return (
+    <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive">
+      <ExclamationTriangleIcon className="h-4 w-4" />
+      <p>{message}</p>
+    </div>
+  );
 };
 
 export default ErrorMessage;
--- app/components/success-message.tsx
+++ app/components/success-message.tsx
@@ -1,11 +1,17 @@
-import React, { FC } from 'react';
+import { CheckCircledIcon } from '@radix-ui/react-icons';
+import { FC } from 'react';
 
-type TSuccessMessageProps = {
+type TErrorMessageProps = {
   message: string;
 };
 
-const SuccessMessage: FC<TSuccessMessageProps> = ({ message }) => {
-  return <div className="text-emerald-600">{message}</div>;
+const ErrorMessage: FC<TErrorMessageProps> = ({ message }) => {
+  return (
+    <div className="bg-emerald-500/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-emerald-500">
+      <CheckCircledIcon className="h-4 w-4" />
+      <p>{message}</p>
+    </div>
+  );
 };
 
-export default SuccessMessage;
+export default ErrorMessage;

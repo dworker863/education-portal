7b4ea301595126062b21a1402c93ce372ccc98a7
dworker <dworker863@gmail.com>
2025-03-09 11:55:46 +0500

    Added option to transform content string with html tags to html

--- app/components/lesson-card.tsx
+++ app/components/lesson-card.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import React, { FC } from 'react';
 import { IExercise, ILesson } from '../libs/interfaces/interfaces';
 import Video from './video';
 import Exercise from './exercise';
+import DOMPurify from 'dompurify';
 
 type TLessonCardProps = {
   lesson: ILesson | null;
@@ -9,11 +12,13 @@ type TLessonCardProps = {
 };
 
 const LessonCard: FC<TLessonCardProps> = ({ lesson, exercise }) => {
+  const content = lesson?.content ? DOMPurify.sanitize(lesson?.content) : '';
+
   return (
     <div className="flex w-full gap-10 p-10 bg-primary text-primary-foreground rounded-lg">
       <div className="w-2/4">
         <h2 className="mb-5 text-center">Теория</h2>
-        <div className="mb-10">{lesson?.content}</div>
+        <div className="mb-10" dangerouslySetInnerHTML={{ __html: content }} />
         {lesson?.video && <Video src={lesson?.video} />}
       </div>
       <div className="w-2/4">{exercise && <Exercise exercise={exercise} />}</div>
